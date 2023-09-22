182c54edebfe7418118f72d51a5aa1d81e714120
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-09-22 18:36:22 +0800

    增加 Swagger3 与 SpringBoot3 集成内容

--- test-swagger3-with-springboot3/src/main/java/com/test/swagger/OpenAPIConfig.java
+++ test-swagger3-with-springboot3/src/main/java/com/test/swagger/OpenAPIConfig.java
@@ -3,7 +3,6 @@ package com.test.swagger;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +12,9 @@ public class OpenAPIConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("MyBoot API")
-                        .description("SpringFox-SpringDoc API 演示")
-                        .version("v1")
-                        .license(new License().name("Apache 2.0")
-                                .url("https://gitee.com/heavyHead/Mercury/blob/master/LICENSE")))
+                        .title("测试 title")
+                        .description("SpringBoot3 集成 Swagger3")
+                        .version("v1"))
                 .externalDocs(new ExternalDocumentation()
                         .description("项目API文档")
                         .url("/"));
